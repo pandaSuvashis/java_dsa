ae6fbfc026fe11afd612f2000e812b1d9b057b03
pandaSuvashis <suvashispanda18@gmail.com>
2026-08-06 10:35:20 +0530

    updated some sort of parts

--- lec 32/Main.java
+++ lec 32/Main.java
@@ -24,6 +24,8 @@ public class Main{
     public static void main(String args[]){
         System.out.println("Binary search!!!");
         System.out.println();
+
+        //binary search   
         int arr[] = {1,2,3,4,5,6,7,8,9};
         printArray(arr);
         System.out.println();
